c89da5579c849e8ebd45fdfda2e0c1e9cf9a9450
Jay Patel <pateljay.ram00@gmail.com>
2020-06-10 18:28:44 +0530

    Incremental Commit

--- TheCloudBill.Host/ClientApplication/src/app/app-routing.module.ts
+++ TheCloudBill.Host/ClientApplication/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
@@ -9,7 +10,7 @@ const routes: Routes = [
     path: '',
     component: AppComponent,
     children: [
-
+      {path:'home', component:HomeComponent}
     ]
   }
 
